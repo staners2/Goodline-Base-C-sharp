02b8a6ec7678af182041928d72368dd46c5e58c5
staners2 <dima.aratin@mail.ru>
2021-03-20 07:38:02 +0300

    Добавление ДЗ№2

---
+++ Task_2(19.03.21)/ConsoleApp/ConsoleApp/Task_1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp
+{
+
+    // Дан размер массива и сам целочисленный упорядоченный по возрастанию массив.
+    // Найти количество различных чисел в нем. (Пример: Дано 3 2 2 2 То результатом будет 1)
+    public static class Task_1
+    {
+        public static void Start()
+        {
+            Random rn = new Random();
+            int[] arrayInts = new int[20];
+            for (int i = 0; i < arrayInts.Length; i++)
+            {
+                arrayInts[i] = rn.Next(0,100);
+                Console.Write($"{arrayInts[i]} ");
+            }
+
+            int count = 0;
+            for (int i = 1; i < arrayInts.Length-1; i++)
+            {
+                if (arrayInts[i-1] != arrayInts[i] && arrayInts[i] != arrayInts[i + 1])
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Кол-во: {count}");
+            Console.WriteLine($"Press the key...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_2(19.03.21)/ConsoleApp/ConsoleApp/Task_2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Task_2
+    {
+        // Дан размер квадратной матрицы (двумерного массива) и сама матрица.
+        // Вывести, симметрична ли матрица относительно главной диагонали? (пример: 2 0 1 1 0 То да)
+        public static void Start()
+        {
+            int sizeMatrix;
+            Console.WriteLine("Введите размерность матрицы: ");
+            while (!int.TryParse(Console.ReadLine(), out sizeMatrix))
+            {}
+
+            int[,] Matrix = new int[sizeMatrix, sizeMatrix];
+            for (int i = 0; i < sizeMatrix; i++)
+            {
+                for (int j = 0; j < sizeMatrix; j++)
+                {
+                    Matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            string Result = IsSymmetricalMatrix(Matrix, sizeMatrix) ? "симметричная" : "не симметричная";
+            Console.WriteLine($"Результат: {Result}");
+            Console.WriteLine($"Press the key...");
+            Console.ReadKey();
+        }
+        public static bool IsSymmetricalMatrix(int[,] Matrix, int sizeMatrix)
+        {
+            for (int i = 0; i < sizeMatrix; i++)
+                for (int j = i + 1; j < sizeMatrix; j++)
+                    if (Matrix[j,i] != Matrix[i,j])
+                        return false;
+                return true;
+        }
+    }
+}
